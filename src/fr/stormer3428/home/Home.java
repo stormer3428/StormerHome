c638eca73ebddbf08bee9ec2652e3a046ee58015
PASTOR CHarles <azertyuiopcharles@gmail.com>
2022-05-06 20:14:39 +0200

    0.1.2
    bugfix : fixed a bug occurring when going to a home in a different dimension (cant measure a distance between two locations of different worlds)

--- src/fr/stormer3428/home/Home.java
+++ src/fr/stormer3428/home/Home.java
@@ -110,7 +110,7 @@ public class Home {
 			int timer = StormerHome.i.getConfig().getInt("teleportationDelay");
 			@Override
 			public void run() {
-				if(cancelOnMove && originalLocation.distanceSquared(p.getLocation()) > 1) {
+				if(cancelOnMove && originalLocation.getWorld() != p.getWorld() && originalLocation.distanceSquared(p.getLocation()) > 1) {
 					Message.error(p, Lang.ERROR_MOVED.toString());
 					cancel();
 					return;
